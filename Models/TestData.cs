24e4b6320b8424390d6efdd2ddd5ea86e45ca25b
dbotalov <dbotalov@MSVBOOK>
2022-02-03 23:02:32 +0500

    Add project files.

---
+++ Models/TestData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRTestClient.Models
+{
+    public class TestData
+    {
+        public int Count { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using SRTestClient.Models;
+
+
+namespace SRTestClient
+{
+    internal class Program
+    {
+        static HubConnection hubConnection;
+
+        static async Task Main(string[] args)
+        {
+            await InitSignalRConnection();
+
+            hubConnection.On<object>("send", obj =>
+            {
+                
+            });
+
+
+            
+            
+            // If Stream
+
+            //var stream = hubConnection.StreamAsync<int>("IntStream");
+
+            //if (Console.ReadKey().Key == ConsoleKey.Tab)
+            //{
+            //    await foreach (var item in stream)
+            //    {
+            //        Console.WriteLine($"Iteration items = {item}");
+            //    }
+            //}
+
+            //Console.WriteLine("Stream from server completed");
+
+            
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter);
+        }
+
+        private static Task InitSignalRConnection()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:34235/testData")
+                .WithAutomaticReconnect()
+                .Build();
+
+            return hubConnection.StartAsync();            
+        }
+    }
+}
